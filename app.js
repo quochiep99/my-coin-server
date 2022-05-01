d54b692f9887841236f5d47e016bacc0c2424bbb
Đoàn Quốc Hiệp <doanquochiep32@gmail.com>
2022-05-01 10:51:51 +0700

    install mongoose

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`My-Coin server is listening on port ${port}`)
-})
+  console.log(`My-Coin server is listening on port ${port}`);
+});
