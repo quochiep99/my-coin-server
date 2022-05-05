51f2a36303c889b2a7fbd988f4e127ae25f41dc3
Đoàn Quốc Hiệp <doanquochiep32@gmail.com>
2022-05-05 07:11:09 +0700

    return blocks after new block confirmation

--- controllers/transaction.controller.js
+++ controllers/transaction.controller.js
@@ -5,7 +5,7 @@ const postMineUnconfirmedTransactions = async (req, res) => {
   //
   try {
     const { body } = req;
-    const blocks = await Block.find();
+    let blocks = await Block.find();
     const latestBlock = blocks[blocks.length - 1];
     const newBlock = mineBlock(
       latestBlock.index + 1,
@@ -13,7 +13,8 @@ const postMineUnconfirmedTransactions = async (req, res) => {
       JSON.stringify(body)
     );
     await Block.create(newBlock);
-    res.status(200).json({ message: "Success" });
+    blocks = await Block.find();
+    res.status(200).json({ blocks });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
